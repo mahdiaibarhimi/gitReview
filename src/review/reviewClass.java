2f32c1997e6fa0b7a333b58eaaee2dcd95649b91
mahdia <mahdiaibrahimi@tekschool.us>
2022-11-11 17:00:19 -0500

    second commit

---
+++ src/review/reviewClass.java
@@ -0,0 +1,10 @@
+package review;
+
+public class reviewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is my luckey day inshaAllah");
+	}
+
+}
