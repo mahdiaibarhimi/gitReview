beea49ed675704b911fcbd363c37c3014fa8a180
mahdia <mahdiaibrahimi@tekschool.us>
2022-11-11 23:33:03 -0500

    3commit

--- src/review/reviewClass.java
+++ src/review/reviewClass.java
@@ -5,6 +5,10 @@ public class reviewClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("this is my luckey day inshaAllah");
-	}
 
+
+	}
+public void test() {
+	System.out.println("this is a test");
+}
 }
